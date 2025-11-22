6a5805c16186ab106c984bd9ca1057ff1d77c61e
Darsh-0 <darsh.gandhi@hotmail.co.nz>
2025-11-23 02:27:14 +1300

    changes made

--- src/home.jsx
+++ src/home.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import resumeIcon from './assets/resume.svg'
-import linkedinIcon from './assets/linkedin.svg'
-import githubIcon from './assets/github.svg'
-import gmailIcon from './assets/gmail.svg'
+import resumeIcon from './assets/resume.svg';
+import linkedinIcon from './assets/linkedin.svg';
+import githubIcon from './assets/github.svg';
+import gmailIcon from './assets/gmail.svg';
+import HomeIcon from './HomeIcon';;
+
+import SpotifyManagerIcon from './assets/projects/SpotifyManager.png';
+import CoffeeCatcherIcon from './assets/projects/CoffeeCatcher.png';
+import UCFKIcon from './assets/projects/UCFK.png';
+import TakeAHikeIcon from './assets/projects/TakeAHike.png';
+
+import DarshImage from './assets/aboutme/me.jpg';
 
 function Title() {
     return (
@@ -21,7 +29,7 @@ function Title() {
     );
 }
 
-function ProgrammingLanguages() {
+function ProgrammingLanguages({ hoveredLang, setHoveredLang }) {
   const logos = [
       {image: "https://brandslogos.com/wp-content/uploads/images/large/python-logo.png", url:"https://www.python.org/", name:"Python"},
       {image: "https://brandlogos.net/wp-content/uploads/2021/11/java-logo.png", url:"https://www.java.com/en/", name:"Java"},
@@ -43,13 +51,82 @@ function ProgrammingLanguages() {
       gap-5"
     >
       {logos.map((logo) => (
-        <a href={logo.url} className="flex flex-row items-center bg-[#FFFFFF20] rounded-md w-full transition-transform hover:scale-105 gap-5">
-            <img key={logo.image} src={logo.image} alt={logo.name} className="object-contain h-20 max-w-full transition-transform hover:scale-110 bg-white p-2 rounded-l-md" />
-            <h1 className="font-[Myfont] text-white mr-30">{logo.name}</h1>
+        <a key={logo.name} href={logo.url} onMouseEnter={() => setHoveredLang(logo.name)} onMouseLeave={() => setHoveredLang(null)} className="flex flex-row items-center bg-[#FFFFFF20] rounded-md w-full transition-transform duration-50 hover:scale-105 gap-5">
+            <img key={logo.image} src={logo.image} alt={logo.name} className="object-contain h-20 max-w-full bg-white p-2 rounded-l-md shadow-[10px_0_20px_rgba(0,0,0,0.4)]" />
+            <h1 className="font-[Myfont] text-white mr-30 text-[20px] font-normal">{logo.name}</h1>
         </a>
       ))}
     </div>
   );
+}
+
+function AboutMe() {
+    const gridsclassName = "rounded-lg flex flex-col flex-1 p-5";
+
+    return (
+        <div className="grid grid-cols-3 gap-10 pb-10">
+            <div className={`bg-gradient-to-t from-[#650A00] to-[#913000] ${gridsclassName}`}>
+                <h1 className="pl font-[Myfont] font-medium text-white text-3xl">
+                    Who I Am?
+                </h1>
+                <h1 className="pt-2 font-[Myfont] font-light text-white">
+                    I'm Darsh Gandhi 20 year old Software Engineering student at The {" "}
+                    <a href="https://www.canterbury.ac.nz/"  className="text-blue-400 underline" target="_blank" rel="noopener noreferrer">
+                        University of Canterbury
+                    </a>{" "}
+                    with the Hiranga Scholarship after Completing the International Baccalaureate Program.
+                    I've gotten more into programming and Software Engineering after starting
+                    university and making fun side projects.
+                </h1>
+                <div className="flex justify-center mt-5">
+                    <img src={DarshImage} className="w-40 rounded-md" />
+                </div>
+            </div>
+            <div className={`bg-gradient-to-t from-[#073D00] to-[#275E00] ${gridsclassName}`}>
+                <h1 className="pl font-[Myfont] font-medium text-white text-3xl">
+                    Skills
+                </h1>
+                <h1 className="pt-2 font-[Myfont] font-light text-white">
+                    I'm most confident programming in Python, Java and C.
+                    I have had experience working on projects, developing Apps as groups.
+                    Working with CI/CD pipelines to improve efficiency.
+                    Programming using Microcontrollers and Embedded Systems.
+
+                </h1>
+                <div className="flex justify-center mt-5">
+                    <img src={DarshImage} className="w-40 rounded-md" />
+                </div>
+            </div>
+            <div className={`bg-gradient-to-t from-[#362870] to-[#524893] ${gridsclassName}`}>
+                <h1 className="font-[Myfont] font-medium text-white">hi</h1>
+            </div>
+        </div>
+    );
+}
+
+
+function Projects() {
+    const MyProjects = [
+        {name: "Spotify Manager Bot", image: SpotifyManagerIcon, url:"https://github.com/Darsh-0/Spotify-Manager-Bot"},
+        {name: "Coffee Catcher", image: CoffeeCatcherIcon, url:"https://github.com/Darsh-0/Coffee_Catcher"},
+        {name: "Portofio Website", image: "https://placehold.co/305x305", url:"https://github.com/Darsh-0/MyWebsite"},
+        {name: "Pong On UCFK4 Microcontroller", image: UCFKIcon, url:"https://github.com/Darsh-0/Pong-on-UCFK4"},
+        {name: "TakeAHike", image: TakeAHikeIcon, url:"https://github.com/Darsh-0/TakeAHike"},
+    ];
+    return (
+        <div className="flex flex-row gap-4">
+            {MyProjects.map((project) => (
+                <button key={project.image} className="flex flex-col items-start p-5 max-w-50 rounded-md hover:bg-[#1E1E1E] cursor-pointer flex" onClick={() => window.open(project.url, "_blank")}>
+                    <div className="rounded-lg overflow-hidden w-[170px] h-[170px] bg-[#00000030] overflow-hidden flex justify-center items-center">
+                        <img key={project.image} src={project.image} alt={project.name} className="max-w-[170px] max-h-[170px] object-contain"/>
+                    </div>
+                    <div className="flex flex-col pt-5 text-left">
+                        <h1 className="text-[#A0A0A0] whitespace-normal">{project.name}</h1>
+                    </div>
+                </button>
+            ))}
+        </div>
+    );
 }
 
 function Socials() {
@@ -128,9 +205,8 @@ function Socials() {
   );
 }
 
-function Buttons() {
+function Buttons({selected, setSelected}) {
   const buttons = ["Languages", "About Me", "Projects"];
-  const [selected, setSelected] = useState(0); // null = none selected
 
   return (
     <div className="flex gap-4 pl-10">
@@ -151,21 +227,43 @@ function Buttons() {
   );
 }
 
-
 export default function Home() {
-  return (
-    <div className="flex flex-col h-screen bg-gradient-to-t from-[#121212] to-[#052449] overflow-x-hidden columns-2">
-        <div className="flex flex-col md:flex-row items-center justify-between pr-25">
+    const [selected, setSelected] = useState(0);
+    const [hoveredLang, setHoveredLang] = useState(null);
+    return (
+    <div className="flex flex-col h-screen overflow-x-hidden bg-black border-l-[14px] border-r-[14px]">
+        <div
+          className={`flex flex-col rounded-2xl overflow-hidden h-screen transition-colors duration-800
+            ${hoveredLang === "Python" ? "bg-gradient-to-t from-[#2B2D42] to-[#3A86FF]"
+            : hoveredLang === "Java" ? "bg-gradient-to-t from-[#052449] to-[#2A1400]"
+            : hoveredLang === "C" ? "bg-gradient-to-t from-[#001219] to-[#005F73]"
+            : hoveredLang === "React" ? "bg-gradient-to-t from-[#0A0A0A] to-[#00D8FF]"
+            : hoveredLang === "Tailwind" ? "bg-gradient-to-t from-[#021F2A] to-[#38BDF8]"
+            : hoveredLang === "HTML" ? "bg-gradient-to-t from-[#2F0A00] to-[#FF5722]"
+            : hoveredLang === "JavaScript" ? "bg-gradient-to-t from-[#2D2A00] to-[#F7DF1E]"
+            : hoveredLang === "CSS" ? "bg-gradient-to-t from-[#001B2E] to-[#2965F1]"
+            : selected === 1 ? "bg-gradient-to-t from-[#222222] to-[#121212]"
+            : "bg-gradient-to-t from-[#121212] to-[#052449]"}`}
+        >
+
+        <div className="bg-black p-2">
+            <HomeIcon size={32} color="white" />
+        </div>
+
+        <div className="flex flex-col items-center justify-between pr-25 mx-auto">
             <Title />
         </div>
 
 
 
         <div className="px-10">
-          <Buttons />
-          <div className="p-8">
-            <ProgrammingLanguages />
-          </div>
+            <Buttons selected={selected} setSelected={setSelected} />
+            <div className="p-8">
+                {selected === 0 && <ProgrammingLanguages hoveredLang={hoveredLang} setHoveredLang={setHoveredLang}/>}
+                {selected === 1 && <AboutMe />}
+                {selected === 2 && <Projects />}
+            </div>
+            </div>
         </div>
     </div>
   );
--- src/resume.jsx
+++ src/resume.jsx
@@ -1,9 +1,22 @@
 import { useNavigate } from "react-router-dom";
+import HomeIcon from './HomeIcon';
+import resume from './assets/pdfs/Resume.pdf';
 
 export default function Resume() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold">This is my Resume page!</h1>
+    return (
+        <div className="flex flex-col h-screen bg-black border-l-[14px] border-r-[14px]">
+            <div className="flex flex-col bg-gradient-to-t from-[#121212] to-[#052449] rounded-2xl overflow-hidden flex-1">
+                <div className="bg-black p-2">
+                    <HomeIcon size={32} color="white" />
+                </div>
+                <div className="flex justify-center items-center flex-1 p-10">
+                    <div className="w-full max-w-5xl h-full bg-black/40 backdrop-blur-md rounded-2xl border border-white/10 shadow-2xl overflow-hidden">
+                        <object data={resume} type="application/pdf" width="100%" height="100%" className="w-full h-full">
+                            <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a></p>
+                        </object>
+                    </div>
+                </div>
+      </div>
     </div>
   );
 }
